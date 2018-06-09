a83bf691bae14e92aa7f217ff250db2cf59a54f8
Ryan Boris <neodynia@icloud.com>
2018-06-09 02:39:05 -0400

    test: Add test suite for RSS Feeds

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,12 +31,25 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
+        it ('contain defined URL feeds that are not empty', function() {
+            for (let feed of allFeeds) {
+                expect(feed.url).toBeDefined();
+                expect(feed.url).not.toBe('', "", null);
+            }
+        });
+        
 
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+        it('have defined names that are not empty', function() {
+            for (let feed of allFeeds) {
+                expect(feed.name).toBeDefined();
+                expect(feed.name).not.toBe('', "", null);
+            }
+        });
     });
 
 
